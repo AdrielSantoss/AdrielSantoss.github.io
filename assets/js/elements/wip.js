fd9d20f0f868bf9cdfff04a1af16945d6e268207
Adriel dos Santos <adrieldossantos7@gmail.com>
2024-03-07 21:58:29 -0300

    WIP: Corrige exibição do gif em Working In Progress.

--- assets/js/elements/wip.js
+++ assets/js/elements/wip.js
@@ -9,7 +9,7 @@ export function loadWorkingInProgress() {
                 <h2>Working in progress...</h2>
                 <p><small>Este conteúdo está sendo implementado...</small></p>
                 <br />
-                <img src="../../assets/images/WIP.gif"/>
+                <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc212amIxbmRlejF3c2tmMzMycjF1OXdrbGFkMzI5bXNkYjdicDQ1aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mcsPU3SkKrYDdW3aAU/giphy.gif" alt="Visum GIF">
                 <br/>
                 <p><small>Acompanhe meu trabalho no </small></p>
                 <a target="_blank" href="https://github.com/AdrielSantoss" _blank>GITHUB</a>
